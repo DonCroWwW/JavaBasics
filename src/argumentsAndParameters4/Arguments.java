e57b153b96e1f8096c73293c633267772c6c2add
Don CroW <negurarazvan98@gmail.com>
2020-06-10 15:48:15 +0300

    I reworked the Argument.class

--- src/argumentsAndParameters4/Arguments.java
+++ src/argumentsAndParameters4/Arguments.java
@@ -13,35 +13,31 @@ public class Arguments {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		about();
+		//	about();
 
-		// Exemple of how to use arguments in a terminal
-		// cd D:\Eclipse\"Java Basics"\src\argumentsAndParameters
-		// java D:\Eclipse\"Java Basics"\src\argumentsAndParameters4\Arguments.java 	Ahile / Apolo
-
-
-
-
-
-		//		System.out.println("What's your name? ");
-		//		System.out.println("Hello " + args[0]+"!!!");   		// If we run this program without an Argument it will return a RUNTIME ERROR
-																		// we are able to run the program, but it crashes
-																		// simple ex of compiling error public static VOI[/D]
-		//		System.out.println("Yeah! "+args[1]+ " It also works with multiple ARGUMENTS, sure");
-
+		//runByTerminal(args[0]);// Exemple of how to use arguments in a terminal
 
 	}
 
 
 
 	public static void about()
+
+
 	{
-		System.out.println("\n \n \n An argument is what we pass to a method");
-		System.out.println("The void method si exepecting, but not requiesting an argument");
+		System.out.println("An argument is what we pass to a method");
 		System.out.println("Why do we need arguments? ");
 		System.out.println("To make dynamic programs");
-		System.out.println("What's the most used way TO PASS AN ARGUMENT? !");
-		System.out.println("Probably the arguemtns u pass when u're using System.out.print \n \n \n");
+
+	}
+
+	public static void runByTerminal(String x) //// If we run this program without an Argument it will return a RUNTIME ERROR
+	{
+		System.out.println("What's your name? ");
+		System.out.println("Hello " +x+"!!!");
+
+		//shortcut for terminal run: java D:/git/java/javaBasics/src/argumentsAndParameters4/Arguments.java replaceNameHere
+		//also works with multiple arguments args[1], args[2] etc
 	}
 
 }
