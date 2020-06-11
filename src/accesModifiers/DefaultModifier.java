8fd335ccab7ade97b7159594294447b1eac6b30e
Don CroW <negurarazvan98@gmail.com>
2020-06-11 12:28:59 +0300

    i added some acces modifiers

---
+++ src/accesModifiers/DefaultModifier.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package accesModifiers;
+
+/**
+ * @author DonCroW
+ *
+ */
+public class DefaultModifier extends foundation3.AccesModifiers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		scopeInJava();
+		scopeOfDefault();
+	}
+	public static void scopeOfDefault ()
+	{
+		System.out.print(" \n \t\t DENY  ");  //GLOBAL
+		System.out.print(" \t\t DENY   ");	  //PACKAGE
+		System.out.print(" \t\t ALLOW");	  //CLASS
+		System.out.print(" \t\t ALLOW   ");   //SUBCLASS
+	}
+}
--- src/foundation3/AccesModifiers.java
+++ src/foundation3/AccesModifiers.java
@@ -1,8 +1,9 @@
 /*
-A list of all acces modifiers
+A list of all acces modifier
 to be continued
 and an example for each one
-
+Since this is quite a large and important topic i would dedicate a special package to it
+Also because i think that i could exemplify it better 
  */
 package foundation3;
 
@@ -24,5 +25,15 @@ public class AccesModifiers {
 	public static void about()
 	{
 		System.out.println("An acces modifier dictates who is gonna be able to acces the method/class");
+	}
+	
+	public static void scopeInJava()
+	{
+		System.out.println("\t When we talk about scope in Java we talk about 4 domanins :");
+		System.out.print(" \t\t GLOBAL");
+		System.out.print(" \t\t PACKAGE");
+		System.out.print(" \t\t CLASS");
+		System.out.print(" \t\t SUBCLASS");
+		
 	}
 }
